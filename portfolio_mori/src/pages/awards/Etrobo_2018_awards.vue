c70baced5ef83729133c19fecb236b49c0c6869d
Mori Keigo <yuzumori.keigo@gmail.com>
2020-06-25 22:16:27 +0900

    画面推移作成

--- portfolio_mori/src/pages/awards/Etrobo_2018_awards.vue
+++ portfolio_mori/src/pages/awards/Etrobo_2018_awards.vue
@@ -4,7 +4,19 @@
 
             <v-row>
                 <v-col id="radius">
-                    <div class="6rem">ETロボコン2018年</div>
+                    <h1 id="center">ETロボコン2018年</h1>
+                    <img id="resizeimage" src="../../assets/etrobo/2018/awards/ETrobo_machine.jpg" height="797" width="1195"/>
+                    <h3>ETロボコンとは...</h3>
+
+                    <div>5年後、10年後に世界をリードするエンジニアの育成を目指し、初心者からベテランまで幅広い層が相互に刺激し合える場を提供する。</div>
+                    <div>世界的にも珍しいソフトウエア重視の教育ロボコン（使用するロボットは同じ規格）である。</div>
+                    <div>地区大会を行い優秀チームがチャンピオンシップ大会に出場できる。</div>
+                    <div>他のロボコンと違う点として、 「競技」と「設計書(モデリング)」で勝負することになる。</div>
+                    <div>なぜ、設計書が必要かというと、設計技術を学び開発工程に生かすという目的がある。</div>
+                    <div>そのため、 設計図(モデル図)+競技(走行)=総合得点となる。</div>
+                    <div>
+                        <a href="https://docs.etrobo.jp/information/ET2020_0613_outline.pdf">ETロボコン2020開催概要より抜粋</a>
+                    </div>
                 </v-col>
             </v-row>
         </v-container>
@@ -18,6 +30,11 @@
 </script>
 
 <style scoped>
+
+
+    #resizeimage {
+        width: 100%;
+    }
 
 
     /*スマホ*/
