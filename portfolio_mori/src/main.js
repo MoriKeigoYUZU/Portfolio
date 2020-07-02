dcd6eded1e63e257f01ea413e3cdf2ab52f2ddea
Mori Keigo <yuzumori.keigo@gmail.com>
2020-07-02 22:50:17 +0900

    ETロボコン2018完成

--- portfolio_mori/src/main.js
+++ portfolio_mori/src/main.js
@@ -3,10 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueYoutube from 'vue-youtube';
 
 Vue.config.productionTip = false
 
 new Vue({
+  VueYoutube,
   router,
   store,
   vuetify,
--- portfolio_mori/src/pages/awards/Etrobo_2018_awards.vue
+++ portfolio_mori/src/pages/awards/Etrobo_2018_awards.vue
@@ -29,15 +29,15 @@
 
                 <v-row>
 
-                    <v-col cols="12" sm="6" md="6" lg="6" >
-                        <div id="radius">
-                        <h3 style="text-indent:1em">経緯</h3>
-                        <p >
-                            高校時代は工業高校で、「僕は進学するし、就活なんて知らんわ」と思って大学に入学しました。<br>
-                            しかし、入学してから途端に「なにかしなくては...」と思い、入学式で募集されていてプログラミングができるETロボコンをに参加しました。<br>
-                            高校時代には近隣の大学に通わせていただきC#でUnityをやっていたので、組み込み技術については全くの素人でした。<br>
-                            体験入部的なものでは、3人同期がいましたが、自分以外全滅したため先輩を独り占めしながら組み込み技術、モデルの書き方を教わりました。<br>
-                        </p>
+                    <v-col cols="12" sm="6" md="6" lg="6">
+                        <div id="">
+                            <h3 style="text-indent:1em">経緯</h3>
+                            <p>
+                                高校時代は工業高校で、「僕は進学するし、就活なんて知らない」と思いながら生活をしていました。<br>
+                                しかし、入学してから途端に「なにかしなくては...」と思い、入学式で募集されていてプログラミングができるETロボコンをに参加しました。<br>
+                                高校時代には近隣の大学に通わせていただきC#でUnityをやっていたので、組み込み技術については全くの素人でした。<br>
+                                体験入部的なものでは、3人同期がいましたが、自分以外全滅したため先輩を独り占めしながら組み込み技術、モデルの書き方を教わりました。<br>
+                            </p>
                         </div>
                     </v-col>
                     <v-col cols="12" sm="6" md="6" lg="6">
@@ -57,16 +57,43 @@
                         <!--<h3 style="text-indent:1em">経緯</h3>-->
 
 
-                        <img id="resizeimage1" src="../../assets/etrobo/2018/awards/ETrobo_award.jpg">
+                        <!--<img id="resizeimage1" src="../../assets/etrobo/2018/awards/ETrobo_award.jpg">-->
+
+                        <youtube :video-id="videoId" width="100%"/>
                     </v-col>
                     <v-col cols="12" sm="7" md="7" lg="7">
                         <h3 style="text-indent:1em">全国(決勝)大会</h3>
                         <p>
-                            私の大学は東海地区のあるため、東海地区のプライマー部門に出場しました。<br>
-                            東海地区は、非常に激戦区でありその代わり決勝進出の枠は多いという特徴的な地区でした。<br>
-                            2018年プライマリー部門の競技は"走行タイム点-難所攻略点"で点数が付けられます。<br>
-                            我々チーム八草は、難所を攻略できず決勝進出できないと思っていました。<br>
-                            しかし、モデル図の点数が高く、走行タイムは地区最速で地区大会3位で決勝進出できました。<br>
+                            約一ヶ月の準備期間を経て我々はパシフィコ横浜で開催された全国大会へ行きました。<br>
+                            人生で始めての大きな大会だったのでとても緊張しました。<br>
+
+                            プライマリー部門の決勝大会はモデルの評価はされず、競技のみで順位が付けられます。<br>
+
+                            我々の走行は難所をすべて攻略し、優勝しました。<br>
+                            成功の鍵は、完全制覇するために走行スピードを落としたことだと思います。<br>
+                            優勝するためには2回の走行両方完走することは必須になります。<br>
+                            そのため、速度を上げすぎると前半の転倒する可能性が高くなります。<br>
+                            私達は、リスクを排除して完走する作戦に成功しました。<br>
+                        </p>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols=12 sm=12 md=4 lg=4 xl=4>
+
+
+                        <img id="resizeimage" src="../../assets/etrobo/2018/awards/ETrobo_award.jpg" />
+                    </v-col>
+
+
+                    <v-col cols="12" sm="8" md="8" lg="8">
+                        <h3 style="text-indent:1em">感想</h3>
+                        <p>
+                            ETロボコンに加入した自分が初年度に全国優勝メンバーの一員になれたことはラッキーでした。<br>
+                            決勝チームで予選タイムが最遅だったため、良い意味で会場の期待を裏切ることもできてよかったです。<br>
+                            加入した当時は"なにかしたい"という気持ちで入ったため、組み込みシステムやSI企業がどのようなものか知りませんでした。<br>
+                            しかし、1年のうちから大手企業や同期の大学生と交流できる機会ができ、自分が何に向いているかもわかりました。<br>
+                            来年は自分がチームリーダーになるため、来年も優勝したいと思いました。<br>
                         </p>
                     </v-col>
                 </v-row>
@@ -78,8 +105,20 @@
 </template>
 
 <script>
+
+  import Vue from 'vue'
+
+  import VueYoutube from 'vue-youtube'
+
+  Vue.use(VueYoutube)
+
   export default {
-    name: "Etrobo_2018_awards"
+    name: "Etrobo_2018_awards",
+    data() {
+      return {
+        videoId: '0vd_vBoP5ww?t=3855'
+      }
+    }
   }
 </script>
 
@@ -98,6 +137,11 @@
     /*スマホ*/
     #radius {
         background-color: rgba(204, 204, 204, 0.3);
+        border-radius: 10px 10px 10px 10px;
+    }
+
+    #youtube {
+        background-color: rgba(250, 250, 250, 0.3);
         border-radius: 10px 10px 10px 10px;
     }
 
@@ -126,6 +170,12 @@
         #radius {
             background-color: rgba(204, 204, 204, 0.3);
             border-radius: 10px 10px 10px 10px;
+        }
+
+        #radiusw {
+            background-color: rgba(250, 250, 250, 0.3);
+            border-radius: 10px 10px 10px 10px;
+
         }
 
         #center {
