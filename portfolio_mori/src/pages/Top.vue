e5823bbd590dc4b0962b3d802e9532b0a15bf6d4
Mori Keigo <yuzumori.keigo@gmail.com>
2020-07-18 15:17:05 +0900

    Topカード修正

--- portfolio_mori/src/pages/Top.vue
+++ portfolio_mori/src/pages/Top.vue
@@ -2,206 +2,196 @@
     <v-app>
         <v-container class="lighten-5">
 
-            <v-row>
+            <v-row id="radius">
                 <v-col id="padding">
-                    <v-col id="radius">
 
-                        <v-carousel
-                                cycle
-                                height="400"
-                                wide="30"
-                                hide-delimiter-background
-                                show-arrows-on-hover
-                        >
-                            <v-carousel-item
-                                    v-for="(item,i) in items"
-                                    :key="i"
-                                    :src="item.src"
-                                    reverse-transition="fade-transition"
-                            ></v-carousel-item>
-                        </v-carousel>
-                        <div id="right">
-                            <v-icon class="title">mdi-camera</v-icon>
-                            <span class="font-italic font-weight-light font-weight-bold" style="color: #6E6E6E">Memorial photo</span>
-                        </div>
-                    </v-col>
+                    <v-carousel
+                            cycle
+                            height="400"
+                            wide="30"
+                            hide-delimiter-background
+                            show-arrows-on-hover
+                    >
+                        <v-carousel-item
+                                v-for="(item,i) in items"
+                                :key="i"
+                                :src="item.src"
+                                reverse-transition="fade-transition"
+                        ></v-carousel-item>
+                    </v-carousel>
+                    <div id="right">
+                        <v-icon class="title">mdi-camera</v-icon>
+                        <span class="font-italic font-weight-light font-weight-bold" style="color: #6E6E6E">Memorial photo</span>
+                    </div>
                 </v-col>
             </v-row>
 
-            <v-row>
+            <v-row id="radius">
                 <v-col id="padding">
-                    <v-col id="radius">
-                        <div class="title" id="center">- Profile プロフィール -</div>
-                        <v-col>
-                            <v-row>
-                                <v-col cols=12 sm=5 md=5 lg=5 xl=5>
-                                    <img src="../assets/profile/mori.jpg" id="resizeimage"/>
-                                </v-col>
-                                <v-col>
-                                    <div class="" id="center">学生エンジニア</div>
-                                    <div class="title" id="center">森 慶悟</div>
-                                    <div class="overline" id="center">
-                                        Mori Keigo 1999.12.13<br>
-                                        愛知工業大学<br>
-                                        <a href="https://pluslab.org/">内藤克浩研究室所属</a><br>
-                                        <a href="https://set1.ie.aitech.ac.jp/">システム工学研究会所属</a><br>
-                                        <a href="https://github.com/MoriKeigoYUZU">GitHub</a><br>
-                                        <a href="https://twitter.com/morikeigo_it">Twiter</a><br>
-                                    </div>
-                                    <hr width="100%" noshade id="center">
-                                    <v-row>
-                                        <v-col></v-col>
-                                    </v-row>
-                                    <span id="">
+
+                    <div class="title" id="center">- Profile プロフィール -</div>
+                    <v-col>
+                        <v-row>
+                            <v-col cols=12 sm=5 md=5 lg=5 xl=5>
+                                <img src="../assets/profile/mori.jpg" id="resizeimage"/>
+                            </v-col>
+                            <v-col>
+                                <div class="" id="center">学生エンジニア</div>
+                                <div class="title" id="center">森 慶悟</div>
+                                <div class="overline" id="center">
+                                    Mori Keigo 1999.12.13<br>
+                                    愛知工業大学<br>
+                                    <a href="https://pluslab.org/">内藤克浩研究室所属</a><br>
+                                    <a href="https://set1.ie.aitech.ac.jp/">システム工学研究会所属</a><br>
+                                    <a href="https://github.com/MoriKeigoYUZU">GitHub</a><br>
+                                    <a href="https://twitter.com/morikeigo_it">Twiter</a><br>
+                                </div>
+                                <hr width="100%" noshade id="center">
+                                <v-row>
+                                    <v-col></v-col>
+                                </v-row>
+                                <span id="">
                                     <div>大学入学と同時にETロボコンを始めました。</div>
                                     <div>2年間ロボコンに参加し全国にも行きましたが、やればやるほど自分にあっていないと感じました。</div>
                                     <div>そこで自分にあうモノを探そうと思い立ち、見つけたのがWEBサービスでした。</div>
                                     <div>現在では、名古屋の勉強会やハッカソンにも積極的に参加しています。</div>
                                 </span>
-                                    <v-row>
-                                        <v-col></v-col>
-                                    </v-row>
-                                    <hr width="100%" noshade id="center">
-                                    <v-row>
-                                        <v-col></v-col>
-                                    </v-row>
-                                    <div>Favorite things 好きなこと : 靴収集,ドライブ,創作料理</div>
-                                    <div>Skill スキル : Vue,JavaScript,Go,C++,EV3</div>
-                                    <div>Studying 勉強中 : AWS,PHP</div>
-                                </v-col>
-                            </v-row>
-                        </v-col>
+                                <v-row>
+                                    <v-col></v-col>
+                                </v-row>
+                                <hr width="100%" noshade id="center">
+                                <v-row>
+                                    <v-col></v-col>
+                                </v-row>
+                                <div>Favorite things 好きなこと : 靴収集,ドライブ,創作料理</div>
+                                <div>Skill スキル : Vue,JavaScript,Go,C++,EV3</div>
+                                <div>Studying 勉強中 : AWS,PHP</div>
+                            </v-col>
+                        </v-row>
                     </v-col>
                 </v-col>
             </v-row>
 
-            <v-row>
+            <v-row id="radius">
                 <v-col id="padding">
-                    <v-col id="radius">
-                        <v-col>
-                            <div class="title" id="center">- Awards Received 受賞歴 -</div>
-                        </v-col>
 
-                        <v-col
-                                v-for="(awards, i) in awards"
-                                :key="i"
-                                cols="12"
+                    <v-col>
+                        <div class="title" id="center">- Awards Received 受賞歴 -</div>
+                    </v-col>
+
+                    <v-col
+                            v-for="(awards, i) in awards"
+                            :key="i"
+                            cols="12"
+                    >
+                        <v-hover
+                                v-slot:default="{ hover }"
                         >
-                            <v-hover
-                                    v-slot:default="{ hover }"
+                            <v-card
+                                    :elevation="hover ? 12 : 2"
+                                    class="mx-auto"
+                                    max-width="mx-auto"
+                                    outlined
+                                    @click=""
+                                    :to="awards.to"
                             >
-                                <v-card
-                                        :elevation="hover ? 12 : 2"
-                                        class="mx-auto"
-                                        max-width="mx-auto"
-                                        outlined
-                                        @click=""
-                                        :to="awards.to"
-                                >
-                                    <div class="d-flex flex-no-wrap justify-space-between">
+                                <div class="d-flex flex-no-wrap justify-space-between">
 
-                                        <div>
+                                    <div>
 
-                                            <v-card-subtitle>
-                                                {{ awards.day }}
-                                            </v-card-subtitle>
+                                        <v-card-subtitle>
+                                            {{ awards.day }}
+                                        </v-card-subtitle>
 
 
-                                            <v-card-text
-                                                    id="text_vw"
-                                            >
+                                        <v-card-text
+                                                id="text_vw"
+                                        >
                                         <span class="font-weight-bold">{{ awards.name }}
                                         </span><br>
-                                                {{ awards.description }}
-                                            </v-card-text>
+                                            {{ awards.description }}
+                                        </v-card-text>
 
 
-                                        </div>
-
-                                        <v-avatar
-                                                class="ma-3"
-                                                size="140"
-                                                tile
-                                        >
-                                            <v-img :src="awards.src"></v-img>
-                                        </v-avatar>
                                     </div>
-                                </v-card>
-                            </v-hover>
 
-                        </v-col>
+                                    <v-avatar
+                                            class="ma-3"
+                                            size="140"
+                                            tile
+                                    >
+                                        <v-img :src="awards.src"></v-img>
+                                    </v-avatar>
+                                </div>
+                            </v-card>
+                        </v-hover>
+
                     </v-col>
                 </v-col>
             </v-row>
 
 
-            <v-row>
+            <v-row id="radius">
                 <v-col id="padding">
-                    <v-col id="radius">
 
-                        <v-col>
-                            <div class="title" id="center">- Production 制作物 -</div>
-                        </v-col>
 
-                        <v-col
-                                v-for="(production, i) in production"
-                                :key="i"
-                                cols="12"
+                    <v-col>
+                        <div class="title" id="center">- Production 制作物 -</div>
+                    </v-col>
+
+                    <v-col
+                            v-for="(production, i) in production"
+                            :key="i"
+                            cols="12"
+                    >
+
+                        <v-hover
+                                v-slot:default="{ hover }"
                         >
 
-                            <v-hover
-                                    v-slot:default="{ hover }"
+                            <v-card
+                                    :elevation="hover ? 12 : 2"
+                                    class="mx-auto"
+                                    max-width="mx-auto"
+                                    outlined
+                                    @click=""
+                                    :to="production.to"
                             >
+                                <div class="d-flex flex-no-wrap justify-space-between">
 
-                                <v-card
-                                        :elevation="hover ? 12 : 2"
-                                        class="mx-auto"
-                                        max-width="mx-auto"
-                                        outlined
-                                        @click=""
-                                        :to="production.to"
-                                >
-                                    <div class="d-flex flex-no-wrap justify-space-between">
+                                    <div>
 
-                                        <div>
-
-                                            <v-card-subtitle>
-                                                {{ production.teams }}
-                                            </v-card-subtitle>
+                                        <v-card-subtitle>
+                                            {{ production.teams }}
+                                        </v-card-subtitle>
 
 
-                                            <v-card-text
-                                                    id="text_vw"
-                                            >
+                                        <v-card-text
+                                                id="text_vw"
+                                        >
                                         <span class="font-weight-bold">{{ production.name }}
                                         </span><br>
-                                                {{ production.explanation }}
-                                            </v-card-text>
-
-
-                                        </div>
-
-                                        <v-avatar
-                                                class="ma-3"
-                                                size="140"
-                                                tile
-                                        >
-                                            <v-img :src="production.src"></v-img>
-                                        </v-avatar>
+                                            {{ production.explanation }}
+                                        </v-card-text>
 
 
                                     </div>
-                                </v-card>
-                            </v-hover>
-                        </v-col>
+
+                                    <v-avatar
+                                            class="ma-3"
+                                            size="140"
+                                            tile
+                                    >
+                                        <v-img :src="production.src"></v-img>
+                                    </v-avatar>
+
+
+                                </div>
+                            </v-card>
+                        </v-hover>
                     </v-col>
                 </v-col>
             </v-row>
-
-            <v-row>
-                <v-col></v-col>
-            </v-row>
-
             <!--<v-row id="radius">-->
 
             <!--<v-col>-->
@@ -336,6 +326,7 @@
         /*margin-right: 20px;*/
         background-color: rgba(204, 204, 204, 0.4);
         border-radius: 10px 10px 10px 10px;
+        margin-bottom: 10px;
     }
 
     #center {
@@ -358,7 +349,9 @@
 
 
     #padding {
+
     }
+
 
 
     @media only screen and (min-width: 651px) {
