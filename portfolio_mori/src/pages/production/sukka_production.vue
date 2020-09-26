a7635b9272de518e357d9d565b4cd1a653160ba1
Mori Keigo <yuzumori.keigo@gmail.com>
2020-09-26 13:38:39 +0900

    更新中に変更

--- portfolio_mori/src/pages/production/sukka_production.vue
+++ portfolio_mori/src/pages/production/sukka_production.vue
@@ -1,5 +1,13 @@
 <template>
-    
+    <v-app>
+        <v-container class="lighten-5">
+            <v-row id="radius">
+                <v-col id="padding">
+                    更新中..
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-app>
 </template>
 
 <script>
