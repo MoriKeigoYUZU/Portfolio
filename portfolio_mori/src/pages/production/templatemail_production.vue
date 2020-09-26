fe1887104a4128a8ed673bfe2e739ae870c7d7c3
Mori Keigo <yuzumori.keigo@gmail.com>
2020-09-26 14:48:40 +0900

    テンプレメールのurl追加

--- portfolio_mori/src/pages/production/templatemail_production.vue
+++ portfolio_mori/src/pages/production/templatemail_production.vue
@@ -3,7 +3,8 @@
         <v-container class="lighten-5">
             <v-row id="radius">
                 <v-col id="padding">
-                    更新中..
+                    更新中..<br>
+                    <a href="https://templatemail-3eed5.web.app/">テンプレメール</a><br>
                 </v-col>
             </v-row>
         </v-container>
